bee2bbb2526a9929b78de582d6bb4f52a7fbb156
Razarac00 <orji.john@outlook.com>
2020-10-06 13:17:20 -0500

    Added skeleton class in ts and a form into html for scrawler

--- App/ClientApp/src/app/scrawler/scrawler.component.ts
+++ App/ClientApp/src/app/scrawler/scrawler.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 
-// @Component({
-//     selector: 'app-scrawler',
-//     templateUrl: './scrawler.component.html'
-//   })
+@Component({
+    selector: 'app-scrawler',
+    templateUrl: './scrawler.component.html'
+  })
+  export class ScrawlerComponent {
+      constructor(parameters) {
+          
+      }
+  }
