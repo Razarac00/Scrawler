e6d5af8e953a34d9966f9abe840bdb9c1f2cf8fc
Razarac00 <orji.john@outlook.com>
2020-10-07 13:46:51 -0500

    Began work on controller

---
+++ App/Controllers/ScrawlerController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace App.Controllers
+{
+    // [ApiController]
+    [Route("[controller]")]
+    public class ScrawlerController : ControllerBase
+    {
+        public ScrawlerController() 
+        {
+
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "";
+        }
+
+        [HttpPost]
+        public void Post(string formInput) 
+        {
+            
+        }
+
+    }
+}
